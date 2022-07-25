76853d96b2c997ca47522219d9d5909c0174990a
Aleksandr Belyakov <aleksander.belyakov@gmail.com>
2022-07-25 16:33:38 +0300

    13. Roman to Integer

---
+++ 13.RomanToInteger/src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "MXXIV";
+        System.out.println(solution.romanToInt(s));
+    }
+}
